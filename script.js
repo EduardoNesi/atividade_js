b7c1c1e776d93a8e4c6940f04abc89c045b33950
Eduardo Nesi <eduardonesic@gmail.com>
2022-05-13 21:01:29 -0300

    versao 1.1

---
+++ script.js
@@ -0,0 +1,32 @@
+function teste(){
+    
+    var nome = window.prompt("Informe seu nome!")
+    var idade = window.prompt("Informe sua idade!")
+    window.alert( "Olá," + nome + ". Você tem " + idade + " anos.")
+    document.getElementById("info").innerHTML = "Olá," + nome + ". Você tem " + idade + " anos.";
+
+    }
+function atividade_01_botao_1(){
+    window.alert("Você clicou no botão 1");
+}
+
+function atividade_01_botao_2(){
+    window.alert("Você clicou no botão 2");
+}
+
+function atividade_01_botao_3(){
+    window.alert("Você clicou no botão 3");
+}
+
+function atividade_02_botao(){
+    var nome = window.prompt("Digite seu nome!");
+    var idade = window.prompt("Digite sua idade!");
+    window.alert("Acabei de conhecer " + nome + ", que tem " + idade + " anos de idade!");
+
+}
+function atividade_03_botao(){
+    var numero = Number( window.prompt("Digite um numero"));
+    var numero1 = numero - 1;
+    var numero2 = numero + 1;
+    window.alert("O numero informado pelo usuário foi " + numero + ". Antes do número " + numero +  ", temos o número " + numero1 + ". Após o número " + numero + ", temos o número " + numero2 + ".");
+}
